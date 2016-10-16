2b80649ebe23e4721aded03a92d068b29c164253
WVL <willemvanlishout@gmail.com>
2016-10-16 20:10:12 +0200

    Changed home text generation

--- home.js
+++ home.js
@@ -6,7 +6,7 @@ var x = function(){
 	var index = Math.floor(Math.random() * sentences.length);
 	console.log(typeof sentences);
 	console.log(index)
-	var p = "";
+	var p = "<div id='home'>";
 	for(var a = 6; a < 20; a++){
 		 var brightness = (0.05*a).toString();
 		 var a = a.toString();
@@ -16,7 +16,7 @@ var x = function(){
 		var brightness = 0.05*b;
 		 p = p + "<span style='display: block; color: rgba(255,255,255,"+brightness+"); text-align: center; font-size: " + b + "px;'>" + sentences[index] + "</span>" ;
 		 }
-	p += "<div id='foot'><a href='https://www.youtube.com/watch?v=CURPyCzoKfY' />Open the Light</a></div>"
+	p += "</div><div id='foot'><a href='https://www.youtube.com/watch?v=CURPyCzoKfY' />Open the Light</a></div>"
 	return p;
 }
 
